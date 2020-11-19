17e7edcb4b912fd213ca062783ad35571fbd3414
Ziyuan <zxu25@slb.com>
2020-11-19 21:15:51 +0800

    feat: add validation to return false when user guess number is not 4

---
+++ BullsAndCowsRunner/UserGuessValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BullsAndCowsRunner
+{
+    public class UserGuessValidator
+    {
+        public bool Validate(string guess)
+        {
+            string guessWithoutSpace = guess.Replace(" ", string.Empty);
+            if (guessWithoutSpace.Length != 4)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ BullsAndCowsTest/UserGuessValidatorTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using BullsAndCowsRunner;
+
+namespace BullsAndCowsTest
+{
+    public class UserGuessValidatorTest
+    {
+        [Theory]
+        [InlineData("1 2 3 4")]
+        public void Should_return_true_when_user_guess_4_digitals_seperated_with_space(string userGuess)
+        {
+            // given
+            UserGuessValidator userGuessValidator = new UserGuessValidator();
+            // when
+            bool validateResult = userGuessValidator.Validate(userGuess);
+            // then
+            Assert.True(validateResult);
+        }
+
+        [Theory]
+        [InlineData("1 2 3 4 5")]
+        [InlineData("1 2 3")]
+        [InlineData("")]
+        public void Should_return_false_when_number_of_user_guess_is_not_4(string userGuess)
+        {
+            // given
+            UserGuessValidator userGuessValidator = new UserGuessValidator();
+            // when
+            bool validateResult = userGuessValidator.Validate(userGuess);
+            // then
+            Assert.False(validateResult);
+        }
+    }
+}
