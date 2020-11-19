657509bb740d3456409e7ce49acb877eda00b1da
Jiankun YANG <JYang53@slb.com>
2020-11-19 16:11:14 +0800

    feat: finish test1 and test2

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -5,16 +5,23 @@ namespace BullsAndCows
     public class BullsAndCowsGame
     {
         private readonly SecretGenerator secretGenerator;
+        private readonly string secret = string.Empty;
         public BullsAndCowsGame(SecretGenerator secretGenerator)
         {
             this.secretGenerator = secretGenerator;
+            this.secret = this.secretGenerator.GenerateSecret();
         }
 
         public bool CanContinue => true;
 
-        public string Guess(string guess)
+        public string Judge(string guess)
         {
-            throw new NotImplementedException();
+            return this.Answer(this.secret, guess);
+        }
+
+        private string Answer(string secret, string guess)
+        {
+            return "0A0B";
         }
     }
 }
--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -8,10 +8,46 @@ namespace BullsAndCowsTest
         [Fact]
         public void Should_create_BullsAndCowsGame()
         {
-            var secretGenerator = new SecretGenerator();
+            var secretGenerator = new TestSecreteGenerator();
             var game = new BullsAndCowsGame(secretGenerator);
             Assert.NotNull(game);
             Assert.True(game.CanContinue);
+        }
+
+        [Fact]
+        public void Should_return_0A0B_when_all_digit_and_position_wrong()
+        {
+            //given
+            var secretGenerator = new TestSecreteGenerator();
+            var game = new BullsAndCowsGame(secretGenerator);
+
+            //when
+            var answer = game.Judge("1 2 3 4");
+
+            //then
+            Assert.Equal("0A0B", answer);
+        }
+
+        [Fact]
+        public void Should_return_4A0B_when_all_digit_and_position_right()
+        {
+            //given
+            var secretGenerator = new TestSecreteGenerator();
+            var game = new BullsAndCowsGame(secretGenerator);
+
+            //when
+            var answer = game.Judge("1 2 3 4");
+
+            //then
+            Assert.Equal("4A0B", answer);
+        }
+
+        public class TestSecreteGenerator : SecretGenerator
+        {
+            public override string GenerateSecret()
+            {
+                return "5 6 7 8";
+            }
         }
     }
 }
