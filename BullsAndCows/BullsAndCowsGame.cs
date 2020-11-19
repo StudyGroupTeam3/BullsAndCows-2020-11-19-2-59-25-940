612881a3b1016fb88083c64e4e116d0bd5e50459
ZFan7 <zfan7@slb.com>
2020-11-19 15:54:04 +0800

    feat: 0A0B

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -1,20 +1,30 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
 
 namespace BullsAndCows
 {
     public class BullsAndCowsGame
     {
         private readonly SecretGenerator secretGenerator;
+        private readonly string secret = string.Empty;
+
         public BullsAndCowsGame(SecretGenerator secretGenerator)
         {
             this.secretGenerator = secretGenerator;
+            this.secret = this.secretGenerator.GenerateSecret();
         }
 
         public bool CanContinue => true;
 
         public string Guess(string guess)
         {
-            throw new NotImplementedException();
+            return this.Compare(this.secret, guess);
+        }
+
+        private string Compare(string secret, string guess)
+        {
+            return "0A0B";
         }
     }
 }
