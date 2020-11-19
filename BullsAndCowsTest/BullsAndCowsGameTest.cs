a0c5b2b9c8c98bc47a19cc69a035c9442e65416f
Ruirui Huang <rhuang2@slb.com>
2020-11-19 16:36:25 +0800

    feat: add theroy tests

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -28,15 +28,33 @@ namespace BullsAndCowsTest
             Assert.Equal("4A0B", answer);
         }
 
-        [Fact]
-        public void Should_return_0A0B_when_all_digit_position_Wrong()
+        [Theory]
+        [InlineData("4 3 2 1")]
+        [InlineData("3 4 2 1")]
+        public void Should_return_0A4B_when_all_digit_Wrong(string guess)
         {
             // given
             var secretGenerator = new TestSecretGenerator();
             var game = new BullsAndCowsGame(secretGenerator);
 
             // when
-            string answer = game.Guess("5 6 7 8");
+            string answer = game.Guess(guess);
+
+            // then
+            Assert.Equal("0A4B", answer);
+        }
+
+        [Theory]
+        [InlineData("5 6 7 8")]
+        [InlineData("6 5 8 9")]
+        public void Should_return_0A0B_when_all_digit_position_Wrong(string guess)
+        {
+            // given
+            var secretGenerator = new TestSecretGenerator();
+            var game = new BullsAndCowsGame(secretGenerator);
+
+            // when
+            string answer = game.Guess(guess);
 
             // then
             Assert.Equal("0A0B", answer);
